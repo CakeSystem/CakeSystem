33e80a56471d6d2f2410473c35b17bcd6f05c4e1
CakeSystem <130638116+CakeSystem@users.noreply.github.com>
2023-04-17 15:35:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -461,7 +461,7 @@ change_limit() {
 installapp() {
     if [ -n "$1" ]; then
         VERSION="$1"
-        ORIGIN_EXEC="rustminersystem-${1}"
+        ORIGIN_EXEC="CakeSystem-${1}"
     fi
 
     echo $ORIGIN_EXEC
