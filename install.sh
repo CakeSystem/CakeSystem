d4cb566bba627c8c85fd43f75b896bad910f2234
CakeSystem <130638116+CakeSystem@users.noreply.github.com>
2023-05-04 00:18:13 +0800

    0.9.98

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="0.9.97"
+VERSION="0.9.98"
 DOWNLOAD_HOST="https://github.com/CakeSystem/CakeSystem/raw/main/linux"
 ORIGIN_EXEC="CakeSystem-${VERSION}"
 
@@ -336,6 +336,7 @@ ISSUE() {
     echo "0.9.95"
     echo "0.9.96"
     echo "0.9.97"
+    echo "0.9.98"
 }
 
 filterResult() {
