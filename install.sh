05c8a5fc5e289a350a3cbd795b31860e15641059
CakeSystem <1625000148@qq.com>
2024-05-05 16:54:31 +0800

    3.9.94

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="3.9.93"
+VERSION="3.9.94"
 DOWNLOAD_HOST="https://github.com/CakeSystem/CakeSystem/raw/main/linux"
 ORIGIN_EXEC="CakeSystem-${VERSION}"
 
@@ -381,6 +381,7 @@ ISSUE() {
     echo "3.9.91"
     echo "3.9.92"
     echo "3.9.93"
+    echo "3.9.94"
 }
 
 filterResult() {
